be7a37670b5b1ee88fecb42e3fb854807f47b1b2
Jørgen Jackwitz <jorgen.jackwitz@technipfmc.com>
2023-12-08 17:23:34 +0100

    Day7 cant figure out whats wrong in part 2..

--- src/day7.rs
+++ src/day7.rs
@@ -5,7 +5,13 @@ KK677 28
 KTJJT 220
 QQQJA 483"#;
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Ord)]
+const EXAMPLE_2: &str = r#"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"#;
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 struct Card(char);
 
 impl Card {
@@ -14,7 +20,6 @@ impl Card {
             'A' => 14,
             'K' => 13,
             'Q' => 12,
-            'J' => 11,
             'T' => 10,
             '9' => 9,
             '8' => 8,
@@ -24,6 +29,7 @@ impl Card {
             '4' => 4,
             '3' => 3,
             '2' => 2,
+            'J' => 1,
             _ => 0
         }
     }
@@ -46,35 +52,47 @@ enum HandType {
     Five
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Hand {
     cards : [Card; 5],
     bid : u32,
-    kind : HandType
+    kind : HandType,
+    old_kind : HandType
 }
 
 impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
         if self.kind == other.kind {
             for i in 0..5 {
-                let comp = self.cards[i].cmp(&other.cards[i]);
+                let comp = self.cards[i].partial_cmp(&other.cards[i]).unwrap();
                 if comp != Ordering::Equal {
-                    println!("[{i}] {:?} L:{:?} R:{:?}", comp, self.cards[i], other.cards[i]); 
-                    return Some(comp);
+                    return comp;
                 } 
             }
-            Some(Ordering::Equal)
+            Ordering::Equal
         } else {
-            Some(self.kind.cmp(&other.kind))
+            self.kind.cmp(&other.kind)
         }
     }
 }
 
 impl Hand {
-    fn determine_hand_type(&mut self) {
+    fn determine_hand_type(&mut self, include_jokers: bool) {
         let mut occurances = [0; 5];
+        let mut jokers = 0;
         for i in 0..self.cards.len() {
+            if self.cards[i].0 == 'J' {
+                jokers += 1;
+                continue;
+            }
             if occurances[i] == -1 {continue};
+
             occurances[i] = 1;
             for j in 0..self.cards.len() {
                 if i == j || occurances[j] == -1 { continue };
@@ -84,7 +102,7 @@ impl Hand {
                 }
             }
         }
-        
+
         for count in occurances.iter() {
             use HandType::*;
             let new_kind = match count {
@@ -92,7 +110,7 @@ impl Hand {
                 3 => Three,
                 4 => Four,
                 5 => Five,
-                _ => {continue}
+                _ => { continue }
             };
             match (new_kind, self.kind) {
                 (OnePair, OnePair) => self.kind = TwoPair,
@@ -100,6 +118,42 @@ impl Hand {
                 (Three, OnePair) => self.kind = FullHouse,
                 (new_kind, old_kind) if new_kind > old_kind => self.kind = new_kind,
                 _ => {},
+            }
+        }
+
+        self.old_kind = self.kind;
+        if include_jokers && jokers > 0 {
+            use HandType::*;
+            match self.kind {
+                HighCard => {
+                    match jokers {
+                        1 => self.kind = OnePair,
+                        2 => self.kind = Three,
+                        3 => self.kind = Four,
+                        4 => self.kind = Five,
+                        _ => {}
+                    }
+                }, 
+                OnePair => {
+                    match jokers {
+                        1 => self.kind = Three,
+                        2 => self.kind = Four,
+                        3 => self.kind = Five,
+                        _ => {}
+                    }
+                },
+                TwoPair => self.kind = FullHouse,
+                Three => {
+                    match jokers {
+                        1 => self.kind = Four,
+                        2 => self.kind = Five,
+                        _ => {}
+                    }
+                },
+                Four => {
+                    self.kind = Five
+                },
+                _ => {}
             }
         }
     }
@@ -111,9 +165,10 @@ impl FromStr for Hand {
         let mut hand = Hand {
             cards : [Card::default(); 5],
             bid: 0,
-            kind : HandType::HighCard
+            kind : HandType::HighCard,
+            old_kind : HandType::HighCard
         };
-        let mut parts = s.split(" ");
+        let mut parts = s.split(' ');
         let cards = parts.next().ok_or("missing cards")?;
         for (i, card) in cards.chars().enumerate() {
             if i >= 5 {
@@ -141,17 +196,23 @@ fn parse(input: &str) -> Vec<Hand> {
 
 fn solve_part1(input: &str) -> u32 {
     let mut hands = parse(input);
-    hands.iter_mut().for_each(|h| h.determine_hand_type());
+    hands.iter_mut().for_each(|h| h.determine_hand_type(false));
     hands.sort();
-    for h in &hands {
-        println!("{:?}", h);
-    }
     hands.iter().enumerate().map(|(i, h)| (i+1) as u32 * h.bid).sum()
 }
 
-// fn solve_part2(input: &str) -> u64 {
-//     //parse2(input).solve()
-// }
+fn solve_part2(input: &str) -> u32 {
+    let mut hands = parse(input);
+    hands.iter_mut().for_each(|h| h.determine_hand_type(true));
+    hands.sort();
+
+    for (i, h) in hands.iter().enumerate() {
+        if h.kind != h.old_kind {
+            println!("[{i}] {:?}", h);   
+        }
+    }
+    hands.iter().enumerate().map(|(i, h)| (i+1) as u32 * h.bid).sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -167,25 +228,25 @@ mod tests {
         assert_eq!(res, 6440)
     }
 
-    // #[test]
-    // fn day7_part1() -> Result<(), Box<dyn Error>> {
-    //     let input = get_input(2023, 6)?;
-    //     let res = solve_part1(&input);
-    //     println!("day7 Part1 Result: {res}");
-    //     Ok(())
-    // }
+    #[test]
+    fn day7_part1() -> Result<(), Box<dyn Error>> {
+        let input = get_input(2023, 7)?;
+        let res = solve_part1(&input);
+        println!("day7 Part1 Result: {res}");
+        Ok(())
+    }
 
-    // #[test]
-    // fn day7_part2_test() {
-    //     let res = solve_part2(EXAMPLE);
-    //     assert_eq!(res, 71503);
-    // }
+    #[test]
+    fn day7_part2_test() {
+        let res = solve_part2(EXAMPLE_2);
+        assert_eq!(res, 5905);
+    }
 
-    // #[test]
-    // fn day7_part2() -> Result<(), Box<dyn Error>> {
-    //     let input = get_input(2023, 6)?;
-    //     let res = solve_part2(&input);
-    //     println!("day7 Part2 Result: {res}");
-    //     Ok(())
-    // }
+    #[test]
+    fn day7_part2() -> Result<(), Box<dyn Error>> {
+        let input = get_input(2023, 7)?;
+        let res = solve_part2(&input);
+        println!("day7 Part2 Result: {res}");
+        Ok(())
+    }
 }
