fb20687b3352a5462f2a6b288e28729247bb0715
jorgen <jorgen.jackwitz@gmail.com>
2023-12-14 21:48:03 +0100

    day13 p2 done

--- src/day13.rs
+++ src/day13.rs
@@ -1,3 +1,5 @@
+use std::fmt::Write;
+
 const EXAMPLE: &str = r#"#.##..##.
 ..#.##.#.
 ##......#
@@ -21,39 +23,57 @@ struct Pattern {
     data: Vec<u8>,
 }
 
+impl std::fmt::Display for Pattern {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("W: {} H: {}\n", self.width, self.height))?;
+        for r in 0..self.height {
+            for c in 0..self.width {
+                let chr = self.index( r, c);
+                f.write_char(*chr as char)?;
+            }
+
+            f.write_char('\n')?;
+        }
+        Ok(())
+    }
+}
+
 impl Pattern {
     fn index(&self, row: usize, col: usize) -> &u8 {
         let i = row * self.width + col;
         self.data.get(i).unwrap_or(&0)
     }
 
-    fn find_vertical_mirror(&self) -> usize {
+    fn find_vertical_mirror(&self, use_smudge: bool) -> usize {
         let mut the_smudge = None;
         let mut mirror_cols = Vec::new();
 
         for c in 0..(self.width-1) {
-            let mut any_unequal = false;
+            let mut unequal = 0;
             let mut smudges = 0;
-            let mut smudge = (0, 0, 0);
+            let mut smudge = (0, 0, 0, 0);
 
             for c2 in c+1..self.width {
                 let c1 = c - (c2 - c - 1);
-                //println!("{c}: Checking col {c1} vs {c2}");
+                //print!("{c}: Checking col {c1} vs {c2}");
                 
                 let count_unequal = (0..self.height)
                     .map(|row| (row, self.index(row, c1), self.index(row, c2)))
                     .filter(|(row, e1, e2)| if e1 != e2 {
-                        smudge = (*row, c, c1);
+                        smudge = (*row, c, c1, c2);
                         true
                     } else { false })
                     .count();
                 
                 if count_unequal == 1 {
                     smudges += 1;
+                    //println!(" > Smudge: {:?}", smudge);
+                } else {
+                    //println!("");
                 }
 
                 if count_unequal > 0 {
-                    any_unequal = true;
+                    unequal += 1;
                 }
 
                 if c1 == 0 {
@@ -61,44 +81,57 @@ impl Pattern {
                 }
             }
 
-            if smudges == 1 {
+            if smudges == 1 && unequal == 1{
                 the_smudge = Some(smudge);
             }
 
-            if !any_unequal {
+            if unequal == 0 {
                 mirror_cols.push(c+1);
             }
         }
-        println!("Col Smudge: {:?}", &the_smudge);
-        println!("Col Mirrors: {:?}", &mirror_cols);
+        if let Some(smdg) = the_smudge {
+            //println!("Col Smudge: [{}, {}] {} [{}, {}]", smdg.0, smdg.2, smdg.1, smdg.0, smdg.3);
+            if use_smudge {
+                //println!("Smudge Col Mirrors: {:?}", smdg.1 + 1);
+                return smdg.1 + 1;
+            }
+        }
+        if use_smudge {
+            return 0;
+        }
+
+        //println!("Col Mirrors: {:?}", &mirror_cols);
         *mirror_cols.last().unwrap_or(&0)
     }
 
-    fn find_horizontal_mirror(&self) -> usize {
+    fn find_horizontal_mirror(&self, use_smudge: bool) -> usize {
         let mut the_smudge = None;
         let mut mirror_rows = Vec::new();
 
         for r in 0..(self.height-1) {
-            let mut any_unequal = false;
+            let mut unequal = 0;
             let mut smudges = 0;
-            let mut smudge = (0, 0, 0);
+            let mut smudge = (0, 0, 0, 0);
             for r2 in r+1..self.height {
                 let r1 = r - (r2 - r - 1);
-                
+                //print!("{r}: Checking row {r1} vs {r2}");
                 let count_unequal = (0..self.width)
                     .map(|col: usize| (col, self.index(r1, col), self.index(r2, col)))
                     .filter(|(col, e1, e2)| if e1 != e2 {
-                        smudge = (*col, r, r1);
+                        smudge = (*col, r, r1, r2);
                         true
                     } else { false })
                     .count();
                 
                 if count_unequal == 1 {
                     smudges += 1;
+                    //println!(" > {smudges} Smudge: {:?}", smudge);
+                } else {
+                    //println!("");
                 }
 
                 if count_unequal > 0 {
-                    any_unequal = true;
+                    unequal += 1;
                 }
 
                 if r1 == 0 {
@@ -106,34 +139,38 @@ impl Pattern {
                 }
             }
 
-            if smudges == 1 {
+            if smudges == 1 && unequal == 1 {
                 the_smudge = Some(smudge);
             }
 
-            if !any_unequal {
+            if unequal == 0 {
                 mirror_rows.push(r+1);
             }
         }
-        println!("Row Smudge: {:?}", the_smudge);
-        println!("Row Mirrors: {:?}", &mirror_rows);
+
+        if let Some(smdg) = the_smudge {
+            //println!("Row Smudge: [{}, {}] {} [{}, {}]", smdg.2, smdg.0, smdg.1, smdg.3, smdg.0);
+            if use_smudge {
+                //println!("Smudge Row Mirrors: {:?}", smdg.1 + 1);
+                return smdg.1 + 1;
+            }
+        }
+
+        if use_smudge {
+            return 0;
+        }
+        
+        //println!("Row Mirrors: {:?}", &mirror_rows);
         *mirror_rows.last().unwrap_or(&0)
     }
 
-    fn mirror_value(&self) -> usize {
-        let col_val = self.find_vertical_mirror();
-        println!("Column Mirror: {}", col_val);
-        let row_val = self.find_horizontal_mirror();
-        println!("Row Mirror: {}", row_val);
-        if col_val == 0 && row_val == 0 {
-            for r in 0..self.height {
-                for c in 0..self.width {
-                    print!("{}", *self.index(r, c) as char)
-                }
-                println!("");
-            }
-        }
+    fn mirror_value(&self, use_smudge: bool) -> usize {
+        let col_val = self.find_vertical_mirror(use_smudge);
+        //println!("Column Mirror: {}", col_val);
+        let row_val = self.find_horizontal_mirror(use_smudge);
+        //println!("Row Mirror: {}", row_val);
         let v = col_val + 100 * row_val;
-        println!("Value: {v}");
+        //println!("Value: {v}");
         v
     }
 }
@@ -160,7 +197,12 @@ fn parse(input: &str) -> Vec<Pattern> {
 
 fn solve_part1(input: &str) -> u32 {
     let patterns = parse(input);
-    patterns.iter().map(|pat| pat.mirror_value() as u32).sum()
+    patterns.iter().map(|pat| pat.mirror_value(false) as u32).sum()
+}
+
+fn solve_part2(input: &str) -> u32 {
+    let patterns = parse(input);
+    patterns.iter().map(|pat| {println!("{}", pat); pat.mirror_value(true) as u32}).sum()
 }
 
 #[cfg(test)]
@@ -186,19 +228,17 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn day13_part2_test() {
-    //     let res = solve_part2(10, EXAMPLE);
-    //     assert_eq!(res, 1030);
-    //     let res = solve_part2(100, EXAMPLE);
-    //     assert_eq!(res, 8410);
-    // }
+    #[test]
+    fn day13_part2_test() {
+        let res = solve_part2(EXAMPLE);
+        assert_eq!(res, 400);
+    }
 
-    // #[test]
-    // fn day13_part2() -> Result<(), Box<dyn Error>> {
-    //     let input = get_input(2023, 11)?;
-    //     let res = solve_part2(1_000_000, &input);
-    //     println!("day13 Part2 Result: {res}");
-    //     Ok(())
-    // }
+    #[test]
+    fn day13_part2() -> Result<(), Box<dyn Error>> {
+        let input = get_input(2023, 13)?;
+        let res = solve_part2(&input);
+        println!("day13 Part2 Result: {res}");
+        Ok(())
+    }
 }
